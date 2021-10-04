9596066a4bd74b37aa52e8aec7c86646511c8067
Jason Romero <codenamejason@outlook.com>
2021-10-04 10:35:23 -0400

    update infura id for wallet connect

--- scafethmoon/packages/react-app/src/constants.js
+++ scafethmoon/packages/react-app/src/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-restricted-syntax */
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "5467ec09438942f2a8161a7d056b6e44";
+export const INFURA_ID = "7b0e75d38d424750b92791477924d133";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
